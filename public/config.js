f848847756e2364890b5763a147f281f0d6e8fe7
themanronin <35099500+ThemanRonin@users.noreply.github.com>
2024-07-17 08:36:14 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -5,11 +5,8 @@ window.Config = {
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+ ApiKeys: [
+    'ur2617078-5e858edf957dc625312a3946',
   ],
 
   // 日志天数
